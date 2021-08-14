16218653acd2678beb9f6d1120bfab56367f8ff9
Vadim Zinoviev <stragrus@mail.ru>
2021-08-14 22:53:23 +0300

    edit test

--- src/js/__tests__/validate.test.js
+++ src/js/__tests__/validate.test.js
@@ -1,8 +1,8 @@
 import getValidCreditCard from '../validate';
 
 test.each([
-  ['true for valid user card', '378282246310005', true],
-  ['true for valid user card', '36828224631000', true],
+  ['true for valid user card', '379513078397192', true],
+  ['true for valid user card', '4929162475977819246', true],
   ['false for invalid user card ', '7715964999', false],
   ['false for invalid user card ', '42', false],
   ['false for invalid user card ', '4200003992829919100102929010', false],
